bfcf6c7eb9e222fb001772772539e98cd7fdff97
Adriano Gil <adrianogil.san@gmail.com>
2024-08-07 14:13:48 -0400

    feat: using a target directory for running dgtime commands

--- src/python/sandsoftime/dgtime.py
+++ src/python/sandsoftime/dgtime.py
@@ -1,6 +1,7 @@
 """ Dagger of Time script """
 from pyutils.cli.clitools import run_cmd
 from pyutils.cli.flags import verify_flag
+from pyutils.utils.jsonutils import write_to_file, read_json_file
 
 import os
 import sys
@@ -21,13 +22,12 @@ def get_jobs_data_path():
 def load_data():
     data_file_path = get_jobs_data_path()
 
-    jobs_data = {}
+    jobs_data = {"commands": []}
 
     if not os.path.exists(data_file_path):
         return jobs_data
 
-    with open(data_file_path, 'r') as f:
-        jobs_data = json.load(f)
+    jobs_data = read_json_file(data_file_path)
 
     return jobs_data
 
@@ -39,11 +39,16 @@ def save_data(jobs_data):
         json.dump(jobs_data, f)
 
 
-def save_command(interval_minutes, command):
+def save_command(interval_minutes, command, target_directory):
+    current_time  = datetime.datetime.today().strftime('%Y.%m.%d-%Hh%mm')
     new_command_data = {
         "interval":   int(interval_minutes),
         "command":    command,
-        "start_time": datetime.datetime.today().strftime('%Y.%m.%d-%Hh%mm')
+        "start_time": current_time,
+        "last_time": current_time,
+        "target_directory": target_directory,
+        "times_ran": 0,
+        "errors": []
     }
     jobs_data = load_data()
 
@@ -54,24 +59,38 @@ def save_command(interval_minutes, command):
     save_data(jobs_data)
 
 
-def run_command(target_command):
+def run_command(target_command, target_directory):
     print("# Dagger of Time - Running command: \n%s" % (target_command))
     print("########################################")
-    command_output = run_cmd(target_command)
+    target_command = 'cd "%s" && %s' % (target_directory, target_command)
+    command_output = run_cmd(target_command, load_bashrc=True)
     print(command_output)
     print("")
 
 
 def verify_commands_to_run():
+    jobs_data_path = get_jobs_data_path()
     jobs_data = load_data()
-    today = datetime.datetime.today()
 
     for jobtask in jobs_data['commands']:
-        start_time = datetime.datetime.strptime(jobtask["start_time"], "%Y.%m.%d-%Hh%Mm")
-        minutes = int((today - start_time).seconds / 60)
 
-        if minutes % jobtask["interval"] == 0:
-            run_command(jobtask["command"])
+        today = datetime.datetime.today()
+
+        last_time = datetime.datetime.strptime(jobtask["last_time"], "%Y.%m.%d-%Hh%Mm")
+        last_time_minutes = int((today - last_time).seconds / 60)
+
+        if last_time_minutes > jobtask["interval"]:
+            try:
+                run_command(jobtask["command"], jobtask["target_directory"])
+            except Exception as e:
+                print("Error running command: %s" % jobtask["command"])
+                print(e)
+                jobtask["errors"].append(str(e))
+            jobtask["last_time"] = today.strftime('%Y.%m.%d-%Hh%Mm')
+            jobtask["times_ran"] += 1
+
+    write_to_file(jobs_data_path, jobs_data)
+    print("Update file %s" % jobs_data_path)
 
 
 def list_scheduled_commands():
@@ -103,9 +122,13 @@ if __name__ == "__main__":
         print_usage()
     elif verify_flag(["-l", "--list"]):
         list_scheduled_commands()
-    elif len(sys.argv) == 3:
+    elif len(sys.argv) >= 3:
         interval_minutes = sys.argv[1]
         command = sys.argv[2]
-        save_command(interval_minutes, command)
+        if len(sys.argv) > 3:
+            target_directory = sys.argv[3]
+        else:
+            target_directory = os.getcwd()
+        save_command(interval_minutes, command, target_directory)
     else:
         run_commands_loop()
